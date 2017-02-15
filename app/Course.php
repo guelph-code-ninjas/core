841e8ae7c4e602cc6864b3c19de2b5edab1c5e84
David Blajda <blajda@hotmail.com>
2017-02-15 16:29:36 -0500

    Created Seeders for courses and their relationship to users.

--- app/Course.php
+++ app/Course.php
@@ -4,12 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Course extends Model
 {
     public function persons()
     {
         return $this->belongsToMany('App\User', 'courses_users',
             'course_id', 'user_id')->withPivot('role');
+    }
+
+    // Register a user for a course
+    public function register(User $user, $role) {
+        $this->persons()->attach($user->id,['role' => $role]); 
     }
     // Three  additonal different tables are required for the following 
     // relationships. We can reduce this to one table with a column
