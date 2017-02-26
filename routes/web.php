9768de70a89563821c8eaa78f7f27b08bc5375fa
vagrant <vagrant@homestead>
2017-02-26 16:00:59 +0000

    Not relevant, DO NOT MERGE TO DEV

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('show', 'CarController');
+
+// route to show login form
+//Route::get('login', array('uses' => 'HomeController@showLogin'));
+
+// route to process login form
+//Route::get('login', array('uses' => 'HomeController@doLogin'));
+
+//Route::resource('login', 'HomeController');
 
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
