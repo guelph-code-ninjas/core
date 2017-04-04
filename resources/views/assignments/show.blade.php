fb811e54801ee0df23743e9e73830b758181ba27
Jacky <jakyhatsune@gmail.com>
2017-04-04 17:44:40 -0400

    Added submission area in assignment page

--- resources/views/assignments/show.blade.php
+++ resources/views/assignments/show.blade.php
@@ -24,7 +24,7 @@
                     {{$aDue}}
 
                 </div>
-                <div class="panel-heading" style="font-size: 20px; font-weight: 800">Submission Details</div><hr style="margin:0px">
+                <div class="panel-heading" style="font-size: 20px; font-weight: 800">Submission Details</div>
                 <div class="panel-body">
                     Similarity<br>
                     Errors in files etc.<br>
@@ -36,9 +36,29 @@
                 <div class="panel-heading">Submissions</div>
                 <div class="panel-body">
                     <strong>Previous submissions</strong><br>
-                    <div id="test"></div>
+
+                    <center><button class="btn btn-success" type="button" data-toggle="collapse" data-target="#submission" style="margin-top:10px; margin-bottom:10px; width: 100%">Submit Assignment</button></center>
+
                     <form>
-                        <center><button class="btn btn-primary" type="submit" style="margin-top:10px; width: 100%">Submit New Assignment</button></center>
+                        <div id="submission" class="collapse">
+                            <div class="form-group">
+                                <label for="submissionName">Submission Name</label>
+                                <input type="text" name="sName" class="form-control" value="{{ old('sName') }}" id="submissionName" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputFile">Upload a document</label>
+                                <input type="file" class="form-control-file" id="inputFile" multiple>
+                                <ul id="fileList">
+                                </ul>
+                            </div>
+                            <div class="form-group">
+                                <label for="submissionComments">Submission Comments</label>
+                                <textarea class="form-control" name="sComments" id="submissionComments" placeholder="Description of the assignment..." cols="30" rows="5" required>{{ Input::old('sComments') }}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary" type="submit">Submit</button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
