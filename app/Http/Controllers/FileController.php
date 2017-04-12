d01f8c9405e5dc6a15ef77971512a1692c01c4fe
vagrant <vagrant@homestead>
2017-04-12 20:08:59 +0000

    testing controller

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+include "fileValidator.php"
 
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+	public function test(){
+		//return view('show');
+		print_r($_FILES);
+		echo "<br>";
+		print_r($_POST);  
+	}
+    
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,5 @@
 <?php
 Route::get('/uploadfile','UploadFileController@index');
 Route::post('/uploadfile','UploadFileController@showUploadFile');
+Route::get('/register', 'FileController@test');
 ?>
