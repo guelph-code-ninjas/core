6881857997b51119d705cb2aa89ec243037400e9
Jacky <jakyhatsune@gmail.com>
2017-04-13 14:38:49 -0400

    Added submission page. Connected the submission button on the assignment page to the submission page. Created two redirect buttons on the submission page to direct user back to assignment page and course page. Calculated time difference of submission and due date.

---
+++ resources/views/assignments/submit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel-heading" style="font-size: 20px; font-weight: 400">{{$cName}}: Submission successful!</div>
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+        	<div class="panel panel-default">
+                <div class="panel-heading">Submission for {{$aName}}</div>
+                <div class="panel-body">
+                	<strong>Submission Name:</strong><br>
+                	{{$sName}}<br>
+                	<strong>Files:</strong><br>
+                	<a href="">TEST</a><br>
+                	<strong>Comments:</strong><br>
+                	{{$sComments}}<br>
+                	<strong>Submitted at: </strong><br>
+                	{{$sTime}}<br>
+                	<div style="color:{{$sRemainingColor}}">Submitted {{$sRemaining}}</div><br><br>
+                	<center>
+	                	<div class="form-group" style="display:inline-block">
+	                		<a href="{{action('AssignmentController@show', [$course, $assignment])}}">
+	                        	<button class="btn btn-primary" type="submit">Go back to Assignment</button>
+	                        </a>
+	                    </div>
+	                    <div class="form-group" style="display:inline-block">
+	                        <a href="{{action('CourseController@show', $course)}}">
+	                        	<button class="btn btn-primary" type="submit">Go back to Course</button>
+	                        </a>
+	                    </div>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
